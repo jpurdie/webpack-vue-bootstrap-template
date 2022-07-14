4f170c867d50c44124bdc45a5bdea79d1728b397
Justin Purdie <jpurdie@users.noreply.github.com>
2022-07-14 08:57:23 -0700

    Update paths

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,12 +13,10 @@ module.exports = {
   },
   plugins: [
     'vue',
-    'no-null',
   ],
   rules: {
     'linebreak-style': 'off',
     'no-await-in-loop': 'off',
     'max-len': ['error', { code: 150 }],
-    'no-null/no-null': 2,
   },
 };
